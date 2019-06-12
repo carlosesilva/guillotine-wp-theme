21fed9822993779db1a5f9cd1721f62201d38ced
Carlos E. Silva <casilva@redventures.com>
2019-06-12 16:36:56 -0400

    ACD-2669 | Render react

--- inc/options/src/index.jsx
+++ inc/options/src/index.jsx
@@ -1,1 +1,5 @@
-alert("test");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const domContainer = document.querySelector("#guillotine-options-root");
+ReactDOM.render(<div>All the cool options will go here</div>, domContainer);
